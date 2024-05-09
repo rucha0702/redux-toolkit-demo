4743bfd4951ac3409dee0bd5e6ad52ac5d330a35
rucha0702 <rucha.kasture2002@gmail.com>
2024-05-09 14:49:32 +0530

    chart commented

--- src/pages/charts/LineChart.tsx
+++ src/pages/charts/LineChart.tsx
@@ -1,89 +1,87 @@
 // @ts-ignore
-import CanvasJSReact from '@canvasjs/react-charts';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import CanvasJSReact from '@canvasjs/react-charts';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 
-const fetchData = async ()=>{
-    try{
-        const result = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
-        return result.data;
-    }catch(err){
-        throw err;
-    }
-}
+// const fetchData = async ()=>{
+//     try{
+//         const result = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+//         return result.data;
+//     }catch(err){
+//         throw err;
+//     }
+// }
 
 const LineChart = ()=>{
 
-    const [data,setData] = useState<any>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    // const [data,setData] = useState<any>([]);
+    // const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    useEffect(()=>{
-        const fetchChartData = async()=>{
-            setIsLoading(true);
-            try{
-                const result = await fetchData();
-                setData(result);
-            }
-            catch(error){
-                throw error;
-            }
-            finally{
-                setIsLoading(false);
-            }
-        }
+    // useEffect(()=>{
+    //     const fetchChartData = async()=>{
+    //         setIsLoading(true);
+    //         try{
+    //             const result = await fetchData();
+    //             setData(result);
+    //         }
+    //         catch(error){
+    //             throw error;
+    //         }
+    //         finally{
+    //             setIsLoading(false);
+    //         }
+    //     }
 
-        fetchChartData();
-    },[])
+    //     fetchChartData();
+    // },[])
 
-    useEffect(()=>{
+    // useEffect(()=>{
 
-    },[data])
+    // },[data])
 
-    const CanvasJSChart = CanvasJSReact.CanvasJSChart;
-    const objectToArray = (obj: any) => {
-        return Object.keys(obj).map(key => {
-            // console.log("key",key);
-            return { label: key, y: obj[key] };
-        });
-    };
-    const options = {
-        animationEnabled: true,
-        exportEnabled: true,
-        theme: "light2", // "light1", "dark1", "dark2"
-        title:{
-            text: "Covid cases fluctuation"
-        },
-        axisY: {
-            title: "Number of cases",
-            suffix: ""
-        },
-        axisX: {
-            title: "Date",
-            prefix: "D",
-            interval: 1
-        },
-        data: [{
-            type: "line",
-            toolTipContent: "{label}: {y}",
-            dataPoints:data.cases ? objectToArray(data.cases):[
-                { label: "1", y: 64 },
-                { label: "2", y: 61 },
-                { label: "3", y: 64 },
-            ]
-        }]
-    }
+    // const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+    // const objectToArray = (obj: any) => {
+    //     return Object.keys(obj).map(key => {
+    //         return { label: key, y: obj[key] };
+    //     });
+    // };
+    // const options = {
+    //     animationEnabled: true,
+    //     exportEnabled: true,
+    //     theme: "light2", 
+    //     title:{
+    //         text: "Covid cases fluctuation"
+    //     },
+    //     axisY: {
+    //         title: "Number of cases",
+    //         suffix: ""
+    //     },
+    //     axisX: {
+    //         title: "Date",
+    //         prefix: "D",
+    //         interval: 1
+    //     },
+    //     data: [{
+    //         type: "line",
+    //         toolTipContent: "{label}: {y}",
+    //         dataPoints:data.cases ? objectToArray(data.cases):[
+    //             { label: "1", y: 64 },
+    //             { label: "2", y: 61 },
+    //             { label: "3", y: 64 },
+    //         ]
+    //     }]
+    // }
 
     return(
-        <div>Line Chart
+        <div>Line Chart component giving deployment error, temporarily commented the code
 
-            {
+            {/* {
                 isLoading?<div className='text-red-600 h1'>Loading...</div>:<div></div>
-            }
+            } */}
             <div>
                 
-            <CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
+            {/* <CanvasJSChart options = {options}
+			/> */}
             </div>
         </div>
     )
